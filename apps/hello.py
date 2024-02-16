3bcc5172a4c83ecd6bd68e329a90fa482ffdafa2
Anna Androvitsanea <anna.androvitsanea@gmail.com>
2024-02-16 13:11:22 +0100

    Add script for pr-workflow.

---
+++ apps/hello.py
@@ -0,0 +1,10 @@
+import datetime
+
+def main():
+   print("Hello world")
+
+   current_time = datatime.datetime.now()
+   print(f"It is {current_time} o'clock.")
+
+if __name__ == '__main__':
+   main()
